e2ab899e0d5e668a173c21b2949da4176b1036db
xpetomichalik <michalik.peter99@gmail.com>
2023-04-15 13:09:25 +0200

    first commit

---
+++ my-app/src/components/ButtonBox.js
@@ -0,0 +1,4 @@
+const ButtonBox = ({ children }) => {
+  return <div className="buttonBox">{children}</div>;
+};
+export default ButtonBox;
---
+++ my-app/src/components/Screen.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+/*import { Textfit } from "react-textfit";*/
+
+const Screen = () => {
+  const { calc } = useContext(CalcContext);
+  return <div className="screen">6969</div>;
+};
+export default Screen;
